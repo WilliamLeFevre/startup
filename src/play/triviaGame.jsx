cab6b4170b86cc87801ad547bd70690623c2c201
William LeFevre <wjl30500@gmail.com>
2026-03-02 10:19:12 -0700

    Moved game input and game info into the TriviaResponse component

--- src/play/triviaGame.jsx
+++ src/play/triviaGame.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { TriviaQuestion } from "./triviaQuestion"
+import { TriviaResponse } from "./triviaResponse"
 
 export function TriviaGame() {
     const [gameRunning, setGameRunning] = React.useState(false)
@@ -9,18 +10,7 @@ export function TriviaGame() {
         style={{height: "50vh"}}>
             <TriviaQuestion />
             
-            <div className="mb-3">Seconds left: 15</div>
-            
-            <div className="d-flex w-100 mb-4">
-                <input type="text" className="form-control me-2 mt-5" placeholder="Answer" />
-                <button className="btn btn-danger mt-5">Skip</button>
-            </div>
-            
-            <div className="w-100 d-flex justify-content-between">
-                <div>Score: 0</div>
-                <button className="btn btn-primary">Submit</button>
-                <div>Skips Left: 3</div>
-            </div>
+            <TriviaResponse />
         </div>
     )
 }
---
+++ src/play/triviaResponse.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export function TriviaResponse() {
+
+    return (
+        <>
+        <div className="mb-3">Seconds left: 15</div>
+            
+        <div className="d-flex w-100 mb-4">
+            <input type="text" className="form-control me-2 mt-5" placeholder="Answer" />
+            <button className="btn btn-danger mt-5">Skip</button>
+        </div>
+        
+        <div className="w-100 d-flex justify-content-between">
+            <div>Score: 0</div>
+            <button className="btn btn-primary">Submit</button>
+            <div>Skips Left: 3</div>
+        </div>
+        </>
+    )
+}
