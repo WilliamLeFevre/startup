6f99f6e2c52aebed909782f29e8ff41b4ff611c4
William LeFevre <wjl30500@gmail.com>
2026-03-01 21:06:43 -0700

    Created authState.js file

---
+++ src/login/authState.js
@@ -0,0 +1,9 @@
+export class AuthState {
+    static Unknown = new AuthState("unkown");
+    static Authenticated = new AuthState("authenticated");
+    static Unauthenticated = new AuthState("unauthenticated");
+
+    constructor(name) {
+        this.name = name;
+    }
+}
