b972f1f2f937771367a4358cb2f26776941908ba
William LeFevre <wjl30500@gmail.com>
2026-03-02 10:09:13 -0700

    Moved question and answer box to separte TriviaGame component

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { OnlineUsers } from "./onlineUsers";
+import { TriviaGame } from "./triviaGame";
 
 export function Play({userName}) {
     const [highScore, setHighScore] = React.useState(localStorage.getItem("highScore") || 0)
@@ -14,21 +15,7 @@ export function Play({userName}) {
                     <OnlineUsers  userName={userName}/>
 
 
-                    <div className="col-12 col-md-10 col-xl-8 d-flex flex-column align-items-center justify-content-center" style={{height: "50vh"}}>
-                        <div className="h3 mb-4 bg-light p-3 border border-dark">Question: How many coins does Mario have to earn before he gets a 1up?</div>
-                        <div className="mb-3">Seconds left: 15</div>
-                        
-                        <div className="d-flex w-100 mb-4">
-                            <input type="text" className="form-control me-2 mt-5" placeholder="Answer" />
-                            <button className="btn btn-danger mt-5">Skip</button>
-                        </div>
-                        
-                        <div className="w-100 d-flex justify-content-between">
-                            <div>Score: 5</div>
-                            <button className="btn btn-primary">Submit</button>
-                            <div>Skips Left: 3</div>
-                        </div>
-                    </div>
+                    <TriviaGame />
 
 
                     <div className="col-2 d-none d-md-block p-3">
