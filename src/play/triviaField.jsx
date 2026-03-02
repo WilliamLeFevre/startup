26f3ad5262a42f7488d23e323dcb7deae9cc864b
William LeFevre <wjl30500@gmail.com>
2026-03-02 13:42:46 -0700

    Completed basic functionality for answering questions correcctly, gaining score, and using skips

--- src/play/triviaField.jsx
+++ src/play/triviaField.jsx
@@ -1,24 +1,65 @@
 import React from "react";
 
 export function TriviaField() {
+    const [question, setQuestion] = React.useState("")
+    const [answer, setAnswer] = React.useState("")
+    const [time, setTime] = React.useState(15)
+    const [skips, setSkips] = React.useState(3)
+    const [response, setResponse] = React.useState("")
+    const [score, setScore] = React.useState(0)
+
+    React.useEffect(() => {
+        getPrompt()
+        setInterval(() => {
+            updateTimer();
+        }, 1000)
+    }, [])
+
+    function getPrompt() {
+        let randomNum = Math.floor(Math.random() * 5) + 1
+        setQuestion("How many coins does it take for Mario to earn " + randomNum + " 1ups")
+        setAnswer(String(100 * randomNum))
+    }
+
+    function updateTimer() {
+        setTime(time => time - 1)
+    }
+
+    function skip() {
+        if (skips > 0) {
+            getPrompt()
+            setTime(15)
+            setSkips(skips => skips - 1)
+        }
+    }
+
+    function submitResponse() {
+        if (response === answer) {
+            setResponse("")
+            setScore(score => score + 1)
+            getPrompt()
+            setTime(15)
+        }
+    }
 
     return (
         <>
             <div className="h3 mb-4 bg-light p-3 border border-dark">
-                Question: How many coins does Mario have to earn before he gets a 1up?
+                Question: {question}
             </div>
 
-            <div className="mb-3">Seconds left: 15</div>
+            <div className="mb-3">Seconds left: {time}</div>
                 
             <div className="d-flex w-100 mb-4">
-                <input type="text" className="form-control me-2 mt-5" placeholder="Answer" />
-                <button className="btn btn-danger mt-5">Skip</button>
+                <input type="text" className="form-control me-2 mt-5" placeholder="Answer" 
+                value={response} onChange={(e) => setResponse(e.target.value)} />
+                <button className="btn btn-danger mt-5" onClick={() => skip()}>Skip</button>
             </div>
             
             <div className="w-100 d-flex justify-content-between">
-                <div>Score: 0</div>
-                <button className="btn btn-primary">Submit</button>
-                <div>Skips Left: 3</div>
+                <div>Score: {score}</div>
+                <button className="btn btn-primary" onClick={() => submitResponse()}>Submit</button>
+                <div>Skips Left: {skips}</div>
             </div>
         </>
     )
