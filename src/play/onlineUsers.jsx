983b2a11779bb113078f95d0867e67196ed5276e
William LeFevre <wjl30500@gmail.com>
2026-03-02 09:39:47 -0700

    Added userRow display to onlineUsers

--- src/play/onlineUsers.jsx
+++ src/play/onlineUsers.jsx
@@ -1,17 +1,21 @@
 import React from "react"
 
 export function OnlineUsers() {
+    const [users, setUsers] = React.useState([])
+
+
+    const userRows = []
+    for (let i=0; i < users.length; i++) {
+        userRows.push(
+            <li className="fs-5">{users[i]}</li>
+        )
+    }
 
     return (
         <div className="col-2 d-none d-xl-block p-3">
             <h5>Online Users</h5>
             <ul className="list-unstyled text-nowrap border border-dark p-1">
-                <li className="fs-5">#5 ReverseFlash-50</li>
-                <li className="fs-5">#12 Flash-9</li>
-                <li className="fs-5">#2 chickenFlash-2</li>
-                <li className="fs-5">#9 FunnyDog-1</li>
-                <li className="fs-5">#22 ElloMatey13-11</li>
-                <li className="fs-5">#4 BeforeMeCoug-8</li>
+                {userRows}
             </ul>
         </div>
     )
