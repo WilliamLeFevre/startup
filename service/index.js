67f901c8c6af0cc91f9fe9e04615985de288d4a3
William LeFevre <wjl30500@gmail.com>
2026-03-12 20:58:37 -0600

    added updateScores helper function

--- service/index.js
+++ service/index.js
@@ -19,6 +19,30 @@ app.use(`/api`, apiRouter);
 
 
 
+
+
+function updateScores(newScore) {
+    let found = false
+    for (const [i, prevScore] of scores.entries()) {
+        if (newScore.score > prevScore.score) {
+            scores.splice(i, 0, newScore)
+            found = true
+            break
+        }
+    }
+
+    if (!found) {
+        scores.push(newScore);
+    }
+
+    if (scores.length > 10) {
+        scores.length = 10
+    }
+
+    return scores
+}
+
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
