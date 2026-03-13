30b3f4fbc492f7ea3f0983e957ab6be5ad6e3caf
William LeFevre <wjl30500@gmail.com>
2026-03-12 21:18:06 -0600

    Added endpoint to delete user

--- service/index.js
+++ service/index.js
@@ -41,6 +41,15 @@ apiRouter.post("/auth/login", async (req, res) => {
     res.status(401).send({msg: "Unauthorized"})
 })
 
+apiRouter.delete("/auth/logout", async (req, res) => {
+    const user = await findUser("token", req.cookies[authCookieName])
+    if (user) {
+        delete user.token
+    }
+    res.clearCookie(authCookieName)
+    res.status(204).end()
+})
+
 
 
 function updateScores(newScore) {
