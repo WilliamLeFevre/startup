c5befeadf633618104f2c8ae9c386006fdf31b9b
William LeFevre <wjl30500@gmail.com>
2026-04-07 17:33:31 -0600

    Added websocket updateScores

--- service/index.js
+++ service/index.js
@@ -185,6 +185,19 @@ function broadcast(payload) {
     }
 }
 
+function updateScores(newScore) {
+    let found = false
+    for (const [i, prevScore] of scores.entries()) {
+        if (newScore.score > prevScore.score) {
+            scores.splice(i, 0, newScore)
+            found = true
+            break
+        }
+    }
+    if (!found) scores.push(newScore)
+    if (scores.length > 10) scores.length = 10
+    return scores
+}
 
 
 
